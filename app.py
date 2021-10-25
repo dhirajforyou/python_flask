6056d23de228956d3accf990435cf47475139063
dhirajforyou <govind.nitk@gmail.com>
2021-10-25 08:31:43 +0530

    addd dynamic_route
    started considering params from url, as well as get param if not found in url.

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return "Hello world"
+
+
+@app.route("/hello/")
+@app.route("/hello/<string:name>")
+@app.route("/hello/<path:name>")
+def sayHello(name=None):
+    if name:
+        return "Hello {}".format(name)
+    if request.args.get("name"):
+        name = request.args.get("name")
+    else:
+        name = "Stranger"
+    return "Hello {}".format(name)
 
 
 if __name__ == "__main__":
