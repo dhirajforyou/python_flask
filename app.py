584299ee91bbb0d49a03749581d68ffbd1d11301
dhirajforyou <govind.nitk@gmail.com>
2021-12-11 14:44:33 +0530

    added mail functionality and db

--- app.py
+++ app.py
@@ -2,11 +2,16 @@
 import logging
 import os
 from datetime import datetime
+from threading import Thread
 
-from flask import Flask, jsonify, render_template, send_from_directory, g, session, redirect, url_for, flash
+from flask import Flask, jsonify, render_template, send_from_directory, g, session, redirect, url_for, flash, \
+    copy_current_request_context
 from flask_bootstrap import Bootstrap
+from flask_mail import Message, Mail
+from flask_migrate import Migrate
 from flask_moment import Moment
-from flask_wtf import Form
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
 from werkzeug.exceptions import HTTPException
 from wtforms import StringField, SubmitField, RadioField
 from wtforms.validators import DataRequired
@@ -14,6 +19,10 @@ from wtforms.validators import DataRequired
 logger = logging.getLogger(__name__)
 app = Flask(__name__)
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "data.sqlite")
+app.config["SQLALCHEMY_COMMIT_ON_TEARDOWN"] = True
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 # serve bootstrap resources from local.
 app.config["BOOTSTRAP_SERVE_LOCAL"] = True
 if os.path.exists('static/js/lib/moment-with-locales.min.js'):
@@ -21,8 +30,55 @@ if os.path.exists('static/js/lib/moment-with-locales.min.js'):
     app.config["local_moment"] = 'js/lib/moment-with-locales.min.js'
 
 app.config["SECRET_KEY"] = "shhhh... its private."
+
+app.config["MAIL_SERVER"] = "smtp.googlemail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_ADMIN"] = os.environ.get("MAIL_ADMIN")
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+app.config["MAIL_SUBJECT_PREFIX"] = "[My Application]"
+app.config["MAIL_SENDER"] = "My Application <no-reply@gmail.com>"
+
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+mail = Mail(app)
+
+
+def create_message(to, subject, template, **kwargs):
+    logger.info("Sending to %s" % to)
+    recipients = to if isinstance(to, list) else [to]
+    msg = Message(app.config.get("MAIL_SUBJECT_PREFIX") + subject,
+                  sender=app.config["MAIL_SENDER"], recipients=recipients)
+    msg.body = render_template(template + ".txt", **kwargs)
+    msg.html = render_template(template + ".html", **kwargs)
+    return msg
+
+
+def send_email(to, subject, template, **kwargs):
+    # Send mail in sync
+    message = create_message(to, subject, template, **kwargs)
+    logger.debug("Prepared mail message")
+    mail.send(message)
+    logger.debug("Sent mail")
+
+
+def send_async_mail(to, subject, template, **kwargs):
+    # send mail in async
+    message = create_message(to, subject, template, **kwargs)
+    logger.debug("Prepared mail message")
+
+    # copy_current_request_context will add app context
+    @copy_current_request_context
+    def send_message(message):
+        mail.send(message)
+        logger.debug("Mail sent")
+
+    sender = Thread(name='mail_sender', target=send_message, args=(message,))
+    sender.start()
+    logger.debug("Thread started")
 
 
 @app.before_request
@@ -41,16 +97,46 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static/images'), 'favicon.png')
 
 
-class Nameform(Form):
+class Nameform(FlaskForm):
     name = StringField("What is your name", validators=[DataRequired()], render_kw={'autofocus': True})
-    # checkmode = RadioField('checkmode', validators=[DataRequired()], choices=[('in', 'Check-In'), ('out', 'Check-Out')])
+    # checkmode = RadioField('checkmode', validators=[DataRequired()],
+    # choices=[('in', 'Check-In'), ('out', 'Check-Out')])
     submit = SubmitField("Submit")
+
+
+class Role(db.Model):
+    __tablename__ = "roles"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return "<Role: %r>" % self.name
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return "<User %r>" % self.username
 
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
     form = Nameform()
     if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if not user:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session["known"] = False
+            send_async_mail(app.config["MAIL_ADMIN"], "New User", "mail/new_user", user=user)
+            logger.debug("mail part completed")
+        else:
+            session["known"] = True
         old_name = session.get("name")
         if old_name:
             if old_name != form.name.data:
@@ -63,6 +149,7 @@ def index():
         # form.checkmode.data = ''
         return redirect(url_for('index'))
     template_data = {"name": session.get("name", "Stranger"),
+                     "known": session.get("known", False),
                      # "checkmode": session.get("checkmode", None),
                      "form": form}
     return render_template("user.html", **template_data)
