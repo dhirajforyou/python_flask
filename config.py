85977ccd40641a9397da08f6059b7891bc0630f5
dhirajforyou <govind.nitk@gmail.com>
2021-12-11 20:47:12 +0530

    initial commit for app structure

---
+++ config.py
@@ -0,0 +1,47 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "shhhh... its private."
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SUBJECT_PREFIX = "[My Application]"
+    MAIL_SENDER = "My Application <no-reply@gmail.com>"
+
+    @staticmethod
+    def init_app(app):
+        # Config specific initialization can be performed.
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    MAIL_SERVER = "smtp.googlemail.com"
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_ADMIN = os.environ.get("MAIL_ADMIN")
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DEV_DATABASE_URL") or \
+                              "sqlite:///" + os.path.join(basedir, "data-dev.sqlite")
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get("TESTING_DATABASE_URL") or \
+                              "sqlite:///" + os.path.join(basedir, "data-test.sqlite")
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or \
+                              "sqlite:///" + os.path.join(basedir, "data.sqlite")
+
+
+config = {
+    "development": DevelopmentConfig,
+    "production": ProductionConfig,
+    "testing": TestingConfig,
+    "default": DevelopmentConfig
+}
